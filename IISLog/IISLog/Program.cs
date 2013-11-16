d428d8f1dc7ee1d4734a7f71830448996203a76f
yongfa365 <yongfa365@qq.com>
2013-11-16 13:42:07 +0800

    添加功能

--- IISLog/IISLog/Program.cs
+++ IISLog/IISLog/Program.cs
@@ -16,7 +16,7 @@ namespace IISLog
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmFileViewAnalytics());
         }
     }
 }
